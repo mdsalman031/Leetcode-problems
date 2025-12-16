0294ae7458ee94a72cdaf310350a9743afe1adf8
mdsalman031 <155325365+mdsalman031@users.noreply.github.com>
2025-12-16 09:36:35 +0530

    Time: 2 ms (97.62%) | Memory: 45.2 MB (64.34%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,14 +3,16 @@ class Solution {
         int n = s.length();
         if(n == 0) return 0;
         int[] hash = new int[256];
-        int maxLen = 1;
-        for(int i = 0 ; i < n ; i++) {
-            Arrays.fill(hash, 0);
-            for(int j = i ; j < n ; j++) {
-                if(hash[s.charAt(j)] == 1) break;
-                maxLen = Math.max(maxLen, j - i + 1);
-                hash[s.charAt(j)] = 1;
-            }
+        Arrays.fill(hash, -1);
+        int left = 0, right = 0, maxLen = 0;
+        while(right < n) {
+            char item = s.charAt(right);
+            if(hash[item] != -1 && hash[item] >= left) {
+                left = hash[item] + 1;
+            } 
+            maxLen = Math.max(maxLen, right - left + 1);
+            hash[item] = right;
+            right++;
         }
         return maxLen;
     }
