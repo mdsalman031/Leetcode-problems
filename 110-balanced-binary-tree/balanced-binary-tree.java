d83e2c1e18694cbdb60489559ff1537f50df427d
mdsalman031 <155325365+mdsalman031@users.noreply.github.com>
2025-12-25 17:38:07 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (9.40%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -16,20 +16,16 @@
 class Solution {
     private int height(TreeNode node) {
         if(node == null) return 0;
-        
-        return 1 + Math.max(height(node.left), height(node.right));
+
+        int lh = height(node.left);
+        int rh = height(node.right);
+
+        if(lh == -1 || rh == -1) return -1;
+        if(Math.abs(lh - rh) > 1) return -1;
+
+        return 1 + Math.max(lh, rh);
     }
     public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-
-        int lh = height(root.left);
-        int rh = height(root.right);
-        if(Math.abs(lh - rh) > 1) return false;
-
-        boolean left = isBalanced(root.left);
-        boolean right = isBalanced(root.right);
-        if(!left || !right) return false;
-
-        return true;
+        return height(root) != -1;
     }
 }
