5b2275bd7f197e20755e0db1e9234732cd7dfb28
mdsalman031 <155325365+mdsalman031@users.noreply.github.com>
2026-01-03 14:30:35 +0530

    Time: 2 ms (97.12%) | Memory: 47.4 MB (33.93%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -13,23 +13,45 @@
  *     }
  * }
  */
+ class BSTIterator {
+        Stack<TreeNode> stack = new Stack<>();
+        boolean reverse = true;
+        public BSTIterator(TreeNode root, boolean isReverse) {
+                reverse = isReverse;
+                pushAll(root);
+        }
+
+        public boolean hasNext() {
+                return !stack.isEmpty();
+        }
+
+        public int next() {
+                TreeNode temp = stack.pop();
+                if(reverse == true) pushAll(temp.left);
+                else pushAll(temp.right);
+                return temp.val;
+        }
+
+        private void pushAll(TreeNode node) {
+                while(node != null) {
+                        stack.push(node);
+                        if(reverse == true) node = node.right;
+                        else node = node.left;
+                }
+        }
+}
+
 class Solution {
-    List<Integer> inorder = new ArrayList<>();
-    private void inorderTraversal(TreeNode node) {
-        if(node == null) return;
-        inorderTraversal(node.left);
-        inorder.add(node.val);
-        inorderTraversal(node.right);
-    }
     public boolean findTarget(TreeNode root, int k) {
-        inorderTraversal(root);
-        int i = 0, j = inorder.size() - 1;
+        BSTIterator asc = new BSTIterator(root, false);
+        BSTIterator desc = new BSTIterator(root, true);
+
+        int i = asc.next(), j = desc.next();
         while(i < j) {
-            int ele1 = inorder.get(i);
-            int ele2 = inorder.get(j);
-            if(ele1 + ele2 == k) return true;
-            else if(ele1 + ele2 < k) i++;
-            else j--;
+                int sum = i + j;
+                if(sum == k) return true;
+                else if(sum < k) i = asc.next();
+                else j = desc.next();
         }
         return false;
     }
