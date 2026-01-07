7e7eac9f6a12356cf087e0fe5d52ddd6cb8f585e
mdsalman031 <155325365+mdsalman031@users.noreply.github.com>
2026-01-08 02:25:49 +0530

    Time: 4 ms (20.19%) | Memory: 47.9 MB (39.12%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,56 @@
+class Pair {
+    int first, second;
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+class Solution {
+    public void solve(char[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int[][] visited = new int[n][m];
+        Queue<Pair> q = new LinkedList<>();
+
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < m ; j++) {
+                if(i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                    if(mat[i][j] == 'O') {
+                        q.offer(new Pair(i, j));
+                        visited[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+
+        while(!q.isEmpty()) {
+            int row = q.peek().first;
+            int col = q.peek().second;
+            q.poll();
+
+            for(int k = 0 ; k < 4 ; k++) {
+                int nrow = row + drow[k];
+                int ncol = col + dcol[k];
+                
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                        visited[nrow][ncol] == 0) {
+                    if(mat[nrow][ncol] == 'O') {
+                        visited[nrow][ncol] = 1;
+                        q.offer(new Pair(nrow, ncol));
+                    }
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < m ; j++) {
+                if(mat[i][j] == 'O' && visited[i][j] == 0) {
+                    mat[i][j] = 'X';
+                }
+            }
+        }
+    }
+}
