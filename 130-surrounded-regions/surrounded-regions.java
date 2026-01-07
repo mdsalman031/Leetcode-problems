77ae93d2f0bd8c29ea194e87528b65d87ff88e69
mdsalman031 <155325365+mdsalman031@users.noreply.github.com>
2026-01-08 02:30:36 +0530

    Time: 4 ms (20.19%) | Memory: 47.9 MB (23.50%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,22 +1,15 @@
-class Pair {
-    int first, second;
-    public Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
 class Solution {
     public void solve(char[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
         int[][] visited = new int[n][m];
-        Queue<Pair> q = new ArrayDeque<>();
+        Queue<int[]> q = new ArrayDeque<>();
 
         for(int i = 0 ; i < n ; i++) {
             for(int j = 0 ; j < m ; j++) {
                 if(i == 0 || i == n - 1 || j == 0 || j == m - 1) {
                     if(mat[i][j] == 'O') {
-                        q.offer(new Pair(i, j));
+                        q.offer(new int[]{i, j});
                         visited[i][j] = 1;
                     }
                 }
@@ -27,9 +20,8 @@ class Solution {
         int[] dcol = {0, 1, 0, -1};
 
         while(!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().second;
-            q.poll();
+            int[] cur = q.poll();
+            int row = cur[0], col = cur[1];
 
             for(int k = 0 ; k < 4 ; k++) {
                 int nrow = row + drow[k];
@@ -39,7 +31,7 @@ class Solution {
                         visited[nrow][ncol] == 0) {
                     if(mat[nrow][ncol] == 'O') {
                         visited[nrow][ncol] = 1;
-                        q.offer(new Pair(nrow, ncol));
+                        q.offer(new int[]{nrow, ncol});
                     }
                 }
             }
