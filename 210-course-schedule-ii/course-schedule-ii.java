968a98851b003b18783cd0c65f37c00f299a6d12
mdsalman031 <155325365+mdsalman031@users.noreply.github.com>
2026-01-11 09:15:42 +0530

    Time: 4 ms (93.75%) | Memory: 46.9 MB (60.69%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,6 +1,26 @@
 class Solution {
+    private boolean dfsCheck(List<List<Integer>> adj, boolean[] visited, boolean[] pathVisited, Stack<Integer> stack, int node) {
+        visited[node] = true;
+        pathVisited[node] = true;
+
+        for(int adjacent : adj.get(node)) {
+            if(!visited[adjacent]) {
+                if(dfsCheck(adj, visited, pathVisited, stack, adjacent))
+                    return true;
+            }
+            else if(pathVisited[adjacent]) 
+                return true;
+        }
+
+        pathVisited[node] = false;
+        stack.push(node);
+        return false;
+    }
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
+        boolean[] visited = new boolean[numCourses];
+        boolean[] pathVisited = new boolean[numCourses];
+        Stack<Integer> stack = new Stack<>();
         for(int i = 0 ; i < numCourses ; i++) {
             adj.add(new ArrayList<>());
         }
@@ -11,32 +31,16 @@ class Solution {
             adj.get(src).add(dest);
         }
 
-        int[] indegree = new int[numCourses];
         for(int i = 0 ; i < numCourses ; i++) {
-            for(int adjacent : adj.get(i)) {
-                indegree[adjacent]++;
-            }
+            if(!visited[i])
+                if(dfsCheck(adj, visited, pathVisited, stack, i))
+                    return new int[]{};
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0 ; i < numCourses ; i++) {
-            if(indegree[i] == 0) q.add(i);
-        }
-
-        int[] courseOrder = new int[numCourses];
+        int[] courseOder = new int[numCourses];
         int idx = 0;
-        while(!q.isEmpty()) {
-            int node = q.poll();
-            courseOrder[idx++] = node;
+        while(!stack.isEmpty()) courseOder[idx++] = stack.pop();
 
-            for(int adjacent : adj.get(node)) {
-                indegree[adjacent]--;
-                if(indegree[adjacent] == 0) q.add(adjacent);
-            }
-        }
-
-        if(idx != numCourses) return new int[]{};
-
-        return courseOrder;
+        return courseOder;
     }
 }
