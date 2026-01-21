f08976bed4cde142abdbbb4f0e35c81d94fd7e7d
mdsalman031 <155325365+mdsalman031@users.noreply.github.com>
2026-01-22 00:40:22 +0530

    Time: 1 ms (99.85%) | Memory: 47.7 MB (17.21%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,20 +1,37 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int n = grid.length;
-        int[] hash = new int[n*n + 1];
+        // MATH :
+        // x = missing number. y = repeated number
 
-        for(int i = 0 ; i < n ; i++) {
-            for(int j = 0 ; j < n ; j++) {
-                hash[grid[i][j]]++;
+        // Sum equation:
+        // Sum_actual - Sum_expected = y - x -------- eq1
+
+        // Sum of squares equation :
+        // sum_actual_square - sum_expected_square = y^2 - x^2 = (y - x)(y + x) 
+        // eq2 = (y-x)(y+x) / (y-x)
+
+        int size = grid.length;
+        int n = size * size;
+        long sum_actual = 0;
+        long sum_expected = (long) (n * (n + 1) / 2);
+
+        long sum_actual_square = 0;
+        long sum_expected_square = (long) (n*(n + 1)*(2L*n + 1)) / 6;
+
+        for(int i = 0 ; i < size ; i++) {
+            for (int num : grid[i]) {
+                sum_actual += num;
+                sum_actual_square += (long) num * num;
             }
         }
 
-        int[] result = new int[2];
-        for(int i = 1 ; i <= n*n ; i++) {
-            if(hash[i] == 2) result[0] = i;
-            else if(hash[i] == 0) result[1] = i;
-        }
+        long eq1 = sum_actual - sum_expected;
+        long eq2 = (sum_actual_square - sum_expected_square) / eq1;
 
-        return result;
+        // Solving both equations
+        long y = (eq1 + eq2) / 2;
+        long x = eq2 - y;
+
+        return new int[]{(int) y,(int) x};
     }
 }
