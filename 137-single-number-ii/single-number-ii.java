696a64791a98e47b84703a43898fea388cb93a67
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-02-15 23:03:44 +0530

    Time: 7 ms (24.14%) | Memory: 45.9 MB (33.66%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,13 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int bitIndex = 0 ; bitIndex < 32 ; bitIndex++) {
-            int count = 0;
-            for(int num : nums) {
-                if((num & (1 << bitIndex)) != 0) count++;
-            }
-            if(count % 3 != 0) ans = ans | (1 << bitIndex);
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i = 1 ; i < n ; i+=3) {
+            if(nums[i-1] != nums[i]) return nums[i-1];
         }
-        return ans;
+        return nums[n-1];
     }
 }
