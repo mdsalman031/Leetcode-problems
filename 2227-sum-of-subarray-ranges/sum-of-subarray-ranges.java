53eed510e292e7597e4b1fa1c1770fe7f082a203
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-03-20 01:13:56 +0530

    Time: 44 ms (17.72%) | Memory: 46.5 MB (60.85%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,17 +1,79 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        int n = nums.length;
-        long sum = 0;
+    private int[] pse(int[] nums, int n) {
+        int[] prevSmaller = new int[n];
+        Stack<Integer> stack = new Stack<>();
 
         for(int i = 0 ; i < n ; i++) {
-            int min = nums[i];
-            int max = nums[i];
-            for(int j = i ; j < n ; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-                sum += (max - min);
-            }
+            while(!stack.isEmpty() && nums[stack.peek()] >= nums[i]) stack.pop();
+
+            int index = (stack.isEmpty()) ? -1 : stack.peek();
+            stack.push(i);
+            prevSmaller[i] = index;
         }
-        return sum;
+
+        return prevSmaller;
+    }
+    private int[] nse(int[] nums, int n) {
+        int[] nextSmaller = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            while(!stack.isEmpty() && nums[stack.peek()] > nums[i]) stack.pop();
+
+            int index = (stack.isEmpty()) ? n : stack.peek();
+            stack.push(i);
+            nextSmaller[i] = index;
+        }
+
+        return nextSmaller;
+    }
+    private int[] pge(int[] nums, int n) {
+        int[] prevGreater = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0 ; i < n ; i++) {
+            while(!stack.isEmpty() && nums[stack.peek()] <= nums[i]) stack.pop();
+
+            int index = (stack.isEmpty()) ? -1 : stack.peek();
+            stack.push(i);
+            prevGreater[i] = index;
+        }
+
+        return prevGreater;
+    }
+    private int[] nge(int[] nums, int n) {
+        int[] nextGreater = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[i]) stack.pop();
+
+            int index = (stack.isEmpty()) ? n : stack.peek();
+            stack.push(i);
+            nextGreater[i] = index;
+        }
+
+        return nextGreater;
+    }
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        int[] prevSmaller = pse(nums, n);
+        int[] nextSmaller = nse(nums, n);
+        int[] prevGreater = pge(nums, n);
+        int[] nextGreater = nge(nums, n);
+
+        long sumMin = 0, sumMax = 0;
+
+        for(int i = 0 ; i < n ; i++) {
+            int leftMin = i - prevSmaller[i];
+            int rightMin = nextSmaller[i] - i;
+            int leftMax = i - prevGreater[i];
+            int rightMax = nextGreater[i] - i;
+
+            sumMin += (long) leftMin * rightMin * nums[i];
+            sumMax += (long) leftMax * rightMax * nums[i];
+        }
+
+        return sumMax - sumMin;
     }
 }
