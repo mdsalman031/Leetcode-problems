9fb9d2bfc4aaf3fb3c7fa7ebdf01a2860b82a719
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-04-06 04:51:26 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (51.97%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,32 +1,25 @@
 class Solution {
-    private void bfs(int[][] image, int sr, int sc, int orgColor, int newColor) {
-        image[sr][sc] = newColor;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{sr,sc});
+    private void dfs(int[][] image, int r, int c, int orgColor, int newColor) {
+        image[r][c] = newColor;
 
         int n = image.length;
         int m = image[0].length;
         int[] drow = {-1, 0, 1, 0};
         int[] dcol = {0, 1, 0, -1};
 
-        while(!q.isEmpty()) {
-            int[] node = q.poll();
-            int r = node[0];
-            int c = node[1];
-            for(int k = 0 ; k < 4 ; k++) {
-                int nrow = r + drow[k];
-                int ncol = c + dcol[k];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == orgColor) {
-                    image[nrow][ncol] = newColor;
-                    q.add(new int[]{nrow, ncol});
-                }
+        for(int k = 0 ; k < 4 ; k++) {
+            int nrow = r + drow[k];
+            int ncol = c + dcol[k];
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == orgColor) {
+                image[nrow][ncol] = newColor;
+                dfs(image, nrow, ncol, orgColor, newColor);
             }
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         if(image[sr][sc] == newColor) return image;
         
-        bfs(image, sr, sc, image[sr][sc], newColor);
+        dfs(image, sr, sc, image[sr][sc], newColor);
         return image;
     }
 }
