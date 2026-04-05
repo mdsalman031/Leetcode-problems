27dcaff473feae9c400c8a69efa035fb8d0ea8c7
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-04-06 04:46:36 +0530

    Time: 1 ms (41.27%) | Memory: 47 MB (32.88%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,30 +1,32 @@
 class Solution {
-    private void dfs(int[][] image, int[][] ans, int row, int col, int[] drow, int[] dcol, int orgColor, int newColor) {
+    private void bfs(int[][] image, int sr, int sc, int orgColor, int newColor) {
+        image[sr][sc] = newColor;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr,sc});
+
         int n = image.length;
         int m = image[0].length;
-
-        ans[row][col] = newColor;
-
-        for(int k = 0 ; k < 4 ; k++) {
-            int nrow = row + drow[k];
-            int ncol = col + dcol[k];
-
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                image[nrow][ncol] == orgColor && ans[nrow][ncol] != newColor) {
-                dfs(image, ans, nrow, ncol, drow, dcol, orgColor, newColor);
-            }
-        }
-    }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length;
-        int m = image[0].length;
-        int[][] ans = image;
-
         int[] drow = {-1, 0, 1, 0};
         int[] dcol = {0, 1, 0, -1};
 
-        dfs(image, ans, sr, sc, drow, dcol, image[sr][sc], color);
-
-        return ans;
+        while(!q.isEmpty()) {
+            int[] node = q.poll();
+            int r = node[0];
+            int c = node[1];
+            for(int k = 0 ; k < 4 ; k++) {
+                int nrow = r + drow[k];
+                int ncol = c + dcol[k];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == orgColor) {
+                    image[nrow][ncol] = newColor;
+                    q.add(new int[]{nrow, ncol});
+                }
+            }
+        }
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(image[sr][sc] == newColor) return image;
+        
+        bfs(image, sr, sc, image[sr][sc], newColor);
+        return image;
     }
 }
