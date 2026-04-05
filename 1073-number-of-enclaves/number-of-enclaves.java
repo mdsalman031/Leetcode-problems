3b81b07499fd9a205db639d863e6f1524301fa2b
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-04-06 05:13:36 +0530

    Time: 10 ms (29.91%) | Memory: 55.8 MB (71.89%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,24 +1,15 @@
-class Pair {
-    int first, second;
-    public Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
 class Solution {
     public int numEnclaves(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         int[][] visited = new int[n][m];
-        Queue<Pair> q = new ArrayDeque<>();
-        
+        Queue<int[]> q = new LinkedList<>();
+
         for(int i = 0 ; i < n ; i++) {
             for(int j = 0 ; j < m ; j++) {
-                if(i == 0 || i == n - 1 || j == 0 || j == m - 1) {
-                    if(grid[i][j] == 1 && visited[i][j] == 0) {
-                        visited[i][j] = 1;
-                        q.offer(new Pair(i, j));
-                    } 
+                if((i == 0 || i == n - 1 || j == 0 || j == m - 1) && grid[i][j] == 1) {
+                    visited[i][j] = 1;
+                    q.add(new int[]{i, j});
                 }
             }
         }
@@ -27,30 +18,26 @@ class Solution {
         int[] dcol = {0, 1, 0, -1};
 
         while(!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().second;
-            q.poll();
-
+            int[] node = q.poll();
+            int r = node[0];
+            int c = node[1];
             for(int k = 0 ; k < 4 ; k++) {
-                int nrow = row + drow[k];
-                int ncol = col + dcol[k];
-
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                        visited[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                int nrow = r + drow[k];
+                int ncol = c + dcol[k];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && visited[nrow][ncol] == 0) {
                     visited[nrow][ncol] = 1;
-                    q.offer(new Pair(nrow, ncol));
+                    q.add(new int[]{nrow, ncol});
                 }
             }
         }
 
-        int count = 0;
+        int enclaves = 0;
         for(int i = 0 ; i < n ; i++) {
             for(int j = 0 ; j < m ; j++) {
-                if(grid[i][j] != visited[i][j]) {
-                    count++;
-                }
+                if(grid[i][j] == 1 && visited[i][j] == 0) enclaves++;
             }
         }
-        return count;
+        
+        return enclaves;
     }
 }
