b5df14da7bf01de56bfb76e3c21d0cbfb5650283
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-04-07 10:37:34 +0530

    Time: 2 ms (79.35%) | Memory: 48 MB (29.18%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,44 +1,45 @@
 class Solution {
+    private void dfs(int row, int col, char[][] mat, boolean[][] visited, int[] drow, int[] dcol) {
+        visited[row][col] = true;
+        int n = mat.length;
+        int m = mat[0].length;
+        for(int k = 0 ; k < 4 ; k++) {
+            int nrow = row + drow[k];
+            int ncol = col + dcol[k];
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !visited[nrow][ncol] && mat[nrow][ncol] == 'O') {
+                dfs(nrow, ncol, mat, visited, drow, dcol);
+            }
+        }
+    }
     public void solve(char[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        Queue<int[]> q = new ArrayDeque<>();
-
-        for(int i = 0 ; i < n ; i++) {
-            for(int j = 0 ; j < m ; j++) {
-                if(i == 0 || i == n - 1 || j == 0 || j == m - 1) {
-                    if(mat[i][j] == 'O') {
-                        mat[i][j] = '#';
-                        q.offer(new int[]{i, j});
-                    }
-                }
-            }
-        }
-
+        boolean[][] visited = new boolean[n][m];
         int[] drow = {-1, 0, 1, 0};
         int[] dcol = {0, 1, 0, -1};
 
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            int row = cur[0], col = cur[1];
-
-            for(int k = 0 ; k < 4 ; k++) {
-                int nrow = row + drow[k];
-                int ncol = col + dcol[k];
-                
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) {
-                    if(mat[nrow][ncol] == 'O') {
-                        mat[nrow][ncol] = '#';
-                        q.offer(new int[]{nrow, ncol});
-                    }
-                }
+        for(int j = 0 ; j < m ; j++) {
+            if(!visited[0][j] && mat[0][j] == 'O') {
+                dfs(0, j, mat, visited, drow, dcol);
+            }
+            if(!visited[n-1][j] && mat[n-1][j] == 'O') {
+                dfs(n-1, j, mat, visited, drow, dcol);
+            }
+        }
+        for(int i = 0 ; i < n ; i++) {
+            if(!visited[i][0] && mat[i][0] == 'O') {
+                dfs(i, 0, mat, visited, drow, dcol);
+            }
+            if(!visited[i][m-1] && mat[i][m-1] == 'O') {
+                dfs(i, m-1, mat, visited, drow, dcol);
             }
         }
 
         for(int i = 0 ; i < n ; i++) {
             for(int j = 0 ; j < m ; j++) {
-                if(mat[i][j] == 'O') mat[i][j] = 'X';
-                else if(mat[i][j] == '#') mat[i][j] = 'O';
+                if(!visited[i][j] && mat[i][j] == 'O') {
+                    mat[i][j] = 'X';
+                }
             }
         }
     }
