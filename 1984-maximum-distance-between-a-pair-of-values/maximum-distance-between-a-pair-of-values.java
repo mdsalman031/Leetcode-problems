41cdf863595f0507333dc8bf94e48d038c8b2e7a
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-04-19 23:55:23 +0530

    Time: 3 ms (98.64%) | Memory: 89.8 MB (90.51%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
@@ -1,25 +1,19 @@
 class Solution {
-    private int floor(int[] nums, int low, int high, int target) {
-        while(low < high) {
-            int mid = low + (high - low) / 2;
-            if(nums[mid] <= target) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
-    }
     public int maxDistance(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int maxDist = Integer.MIN_VALUE;
+        int maxDist = 0;
         
-        for(int j = 0 ; j < n2 ; j++) {
-            int i = (j < n1) ? floor(nums1, 0, j, nums2[j]) : floor(nums1, 0, n1 - 1, nums2[j]);
-            if(nums1[i] <= nums2[j]) maxDist = Math.max(maxDist, j - i);
+        int i = 0, j = 0;
+        while(i < n1 && j < n2) {
+            if(nums1[i] <= nums2[j]) {
+                maxDist = Math.max(maxDist, j - i);
+                j++;
+            } else {
+                i++;
+            }
         }
 
-        return maxDist != Integer.MIN_VALUE ? maxDist : 0;
+        return maxDist;
     }
 }
