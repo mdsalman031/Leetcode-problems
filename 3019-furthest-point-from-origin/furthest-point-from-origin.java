cd7465b37d185ec4aeeb92608e79deda42788366
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-04-25 05:32:50 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (82.34%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.java
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.java
@@ -1,27 +1,26 @@
 class Solution {
-    private int helper(String moves, int index, int count, int[][] dp) {
-        int n = moves.length();
-        if(index == moves.length()) return Math.abs(count);
-
-        if(dp[index][count + n] != -1) return dp[index][count + n];
-
-        char direction = moves.charAt(index);
-        if(direction == 'L') {
-            return dp[index][count + n] = helper(moves, index + 1, count - 1, dp);
-        } else if(direction == 'R') {
-            return dp[index][count + n] = helper(moves, index + 1, count + 1, dp);
-        } else {
-            int ifLeft = helper(moves, index + 1, count - 1, dp);
-            int ifRight = helper(moves, index + 1, count + 1, dp);
-            return dp[index][count + n] = Math.max(ifLeft, ifRight);
-        }
-    }
     public int furthestDistanceFromOrigin(String moves) {
         int n = moves.length();
-        int[][] dp = new int[n][2 * n + 1];
-        for(int i = 0 ; i < n ; i++)
-            for(int j = 0 ; j < 2 * n + 1 ; j++)
-                dp[i][j] = -1;
-        return helper(moves, 0, 0, dp);
+        int leftCount = 0, rightCount = 0;
+        
+        for(int i = 0 ; i < n ; i++) {
+            char direction = moves.charAt(i);
+            if(direction == 'L') leftCount++;
+            else if(direction == 'R') rightCount++;
+        }
+
+        int dist = 0;
+        boolean isLeftMax = (leftCount > rightCount) ? true : false;
+        for(int i = 0 ; i < n ; i++) {
+            char direction = moves.charAt(i);
+            if(direction == 'L') dist--;
+            else if(direction == 'R') dist++;
+            else {
+                if(isLeftMax) dist--;
+                else dist++;
+            }
+        }
+
+        return Math.abs(dist);
     }
 }
