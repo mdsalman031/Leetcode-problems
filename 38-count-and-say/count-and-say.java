bd133e41eea7b17226d12ae04de1d3a077862204
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-05-01 02:21:46 +0530

    Time: 3 ms (75.26%) | Memory: 45.1 MB (18.91%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -5,19 +5,19 @@ class Solution {
         String prev = countAndSay(n - 1);
 
         int count = 1;
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for(int i = 1 ; i < prev.length() ; i++) {
             if(prev.charAt(i) == prev.charAt(i - 1)) {
                 count++;
             } else {
-                result += String.valueOf(count);
-                result += prev.charAt(i - 1);
+                result.append(String.valueOf(count));
+                result.append(prev.charAt(i - 1));
                 count = 1;
             }
         }
-        result += String.valueOf(count);
-        result += prev.charAt(prev.length() - 1);
+        result.append(String.valueOf(count));
+        result.append(prev.charAt(prev.length() - 1));
 
-        return result;
+        return result.toString();
     }
 }
