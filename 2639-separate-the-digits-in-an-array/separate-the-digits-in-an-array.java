afdf755b4cc25e085e239ac448b63c6a77ef2ee3
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-05-11 06:59:32 +0530

    Time: 8 ms (18.92%) | Memory: 45.8 MB (99.85%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -2,14 +2,16 @@ class Solution {
     public int[] separateDigits(int[] nums) {
         int n = nums.length;
         List<Integer> temp = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
 
         for(int i = 0 ; i < n ; i++) {
-            String num = Integer.toString(nums[i]);
-            int index = 0;
-            while(index < num.length()) {
-                int digit = num.charAt(index) - '0';
-                temp.add(digit);
-                index++;
+            int num = nums[i];
+            while(num > 0) {
+                stack.push(num % 10);
+                num /= 10;
+            }
+            while(!stack.isEmpty()) {
+                temp.add(stack.pop());
             }
         }
 
