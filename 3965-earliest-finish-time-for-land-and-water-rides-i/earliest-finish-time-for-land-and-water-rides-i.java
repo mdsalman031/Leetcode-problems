67dcfe038c3844c33f071320019fad16d0f42537
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-06-02 13:08:42 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (77.78%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,35 +1,27 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int minTime = Integer.MAX_VALUE;
         int n = landStartTime.length;
         int m = waterStartTime.length;
+        int ans = Integer.MAX_VALUE;
+
+        int earlyLandEnding = Integer.MAX_VALUE;
+        for(int i = 0 ; i < n ; i++) 
+            earlyLandEnding = Math.min(earlyLandEnding, landStartTime[i] + landDuration[i]);
+        
+        for(int j = 0 ; j < m ; j++) {
+            int actualStart = Math.max(earlyLandEnding, waterStartTime[j]);
+            ans = Math.min(ans, actualStart + waterDuration[j]);
+        }
+
+        int earlyWaterEnding = Integer.MAX_VALUE;
+        for(int j = 0 ; j < m ; j++) 
+            earlyWaterEnding = Math.min(earlyWaterEnding, waterStartTime[j] + waterDuration[j]);
 
         for(int i = 0 ; i < n ; i++) {
-            int curTime = landStartTime[i] + landDuration[i];
-            for(int j = 0 ; j < m ; j++) {
-                if(curTime >= waterStartTime[j]) {
-                    curTime += waterDuration[j];
-                } else {
-                    curTime = waterStartTime[j] + waterDuration[j];
-                }
-                minTime = Math.min(minTime, curTime);
-                curTime = landStartTime[i] + landDuration[i];
-            }
+            int actualStart = Math.max(earlyWaterEnding, landStartTime[i]);
+            ans = Math.min(ans, actualStart + landDuration[i]);
         }
 
-        for(int i = 0 ; i < m ; i++) {
-            int curTime = waterStartTime[i] + waterDuration[i];
-            for(int j = 0 ; j < n ; j++) {
-                if(curTime >= landStartTime[j]) {
-                    curTime += landDuration[j];
-                } else {
-                    curTime = landStartTime[j] + landDuration[j];
-                }
-                minTime = Math.min(minTime, curTime);
-                curTime = waterStartTime[i] + waterDuration[i];
-            }
-        }
-
-        return minTime;
+        return ans;
     }
 }
