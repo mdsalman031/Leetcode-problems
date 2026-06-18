ac4d0ef0d3f25f9130b1a56ee7fedb3a2b16b131
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-06-18 17:50:24 +0530

    Time: 2 ms (34.12%) | Memory: 71.3 MB (57.80%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -3,23 +3,23 @@ class Solution {
         int n = nums.length;
 
         for(int i = 0 ; i < n ; i++) {
-            if(nums[i] <= 0) {
-                nums[i] = n + 1;
-            }
+            if(nums[i] <= 0 || nums[i] > n) nums[i] = n + 1;
         }
 
         for(int i = 0 ; i < n ; i++) {
-            int num = Math.abs(nums[i]);
-            if(num <= n) {
-                int index = num - 1;
-                if(nums[index] > 0) nums[index] *= -1;
+            int index = Math.abs(nums[i]);
+
+            if(index == n) {
+                nums[0] = -Math.abs(nums[i]);
+            } else if(index < n) {
+                nums[index] = -Math.abs(nums[index]);
             }
         }
 
-        for(int i = 0 ; i < n ; i++) {
-            if(nums[i] >= 0) return i + 1;
+        for(int i = 1 ; i < n ; i++) {
+            if(nums[i] > 0) return i;
         }
 
-        return n + 1;
+        return nums[0] > 0 ? n : n + 1;
     }
 }
