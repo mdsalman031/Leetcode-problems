ec0d0998f36f97fa0d77057ea0bfb5053e00f2d1
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-06-18 16:02:46 +0530

    Time: 2 ms (94.78%) | Memory: 71.8 MB (60.66%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,36 +1,18 @@
 class Solution {
-    private int[] getPrefixProduct(int[] nums, int n) {
-        int[] prefix = new int[n];
-
-        prefix[0] = nums[0];
-        for(int i = 1 ; i < n ; i++) {
-            prefix[i] = prefix[i - 1] * nums[i];
-        }
-
-        return prefix;
-    }
-
-    private int[] getSuffixProduct(int[] nums, int n) {
-        int[] suffix = new int[n];
-
-        suffix[n - 1] = nums[n - 1];
-        for(int i = n - 2 ; i >= 0 ; i--) {
-            suffix[i] = suffix[i + 1] * nums[i];
-        }
-
-        return suffix;
-    }
-
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] prefix = getPrefixProduct(nums, n);
-        int[] suffix = getSuffixProduct(nums, n);
         int[] result = new int[n];
 
+        int prod = 1;
         for(int i = 0 ; i < n ; i++) {
-            int leftProduct = i > 0 ? prefix[i - 1] : 1;
-            int rightProduct = i < n - 1 ? suffix[i + 1] : 1;
-            result[i] = leftProduct * rightProduct;
+            result[i] = prod;
+            prod *= nums[i];
+        }
+
+        prod = 1;
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            result[i] *= prod;
+            prod *= nums[i];
         }
 
         return result;
