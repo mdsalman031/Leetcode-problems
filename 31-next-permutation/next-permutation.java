0d4d239db6137801e9a559a689afd4970a491df5
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-06-18 13:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (50.14%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,35 +1,34 @@
 class Solution {
-    private void reverse(int[] nums, int start, int end) {
-        int i = start, j = end;
+    private void reverse(int[] nums, int i, int j) {
         while(i < j) {
             int temp = nums[i];
             nums[i] = nums[j];
             nums[j] = temp;
-            i++;
-            j--;
+            i++; j--;
         }
     }
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int index = -1;
+        int pivot = -1;
+
         for(int i = n - 2 ; i >= 0 ; i--) {
             if(nums[i] < nums[i + 1]) {
-                index = i;
+                pivot = i;
                 break;
             }
         }
-        if(index != -1) {
-            for(int i = n - 1 ; i >= index ; i--) {
-                if(nums[i] > nums[index]) {
+        if(pivot != -1) {
+            for(int i = n - 1 ; i >= 0 ; i--) {
+                if(nums[i] > nums[pivot]) {
                     int temp = nums[i];
-                    nums[i] = nums[index];
-                    nums[index] = temp;
+                    nums[i] = nums[pivot];
+                    nums[pivot] = temp;
                     break;
                 }
             }
-            reverse(nums, index + 1, n - 1);
+            reverse(nums, pivot + 1, n - 1);
         } else {
             reverse(nums, 0, n - 1);
-        }
+        } 
     }
 }
