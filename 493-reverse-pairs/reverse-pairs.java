98555bb6b895e860f3b0b874c0972ecfd7680edc
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-06-29 21:00:36 +0530

    Time: 77 ms (24.28%) | Memory: 59.1 MB (77.51%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,8 +1,7 @@
 class Solution {
     private void merge(int[] nums, int low, int mid, int high) {
-        int left = low;
-        int right = mid + 1;
         List<Integer> temp = new ArrayList<>();
+        int left = low, right = mid + 1;
 
         while(left <= mid && right <= high) {
             if(nums[left] <= nums[right]) {
@@ -28,26 +27,30 @@ class Solution {
             nums[i] = temp.get(i - low);
         }
     }
-    private int countPairs(int[] nums, int low, int mid, int high) {
-        int count = 0;
-        int right = mid + 1;
-        for(int i = low ; i <= mid ; i++) {
-            while(right <= high && nums[i] > 2L * nums[right]) right++;
-            count += (right - (mid + 1));
+
+    private long mergeSort(int[] nums, int low, int high) {
+        long count = 0;
+        if(low < high) {
+            int mid = low + (high - low) / 2;
+            count += mergeSort(nums, low, mid);
+            count += mergeSort(nums, mid + 1, high);
+            count += countReversePairs(nums, low, mid, high);
+            merge(nums, low, mid, high);
         }
         return count;
     }
-    private int mergeSort(int[] nums, int low, int high) {
-        int count = 0;
-        if(low >= high) return count;
-        int mid = (low + high) / 2;
-        count += mergeSort(nums, low, mid);
-        count += mergeSort(nums, mid + 1, high);
-        count += countPairs(nums, low, mid, high);
-        merge(nums, low, mid, high);
+
+    private long countReversePairs(int[] nums, int low, int mid, int high) {
+        int right = mid + 1;
+        long count = 0;
+        for(int i = low ; i <= mid ; i++) {
+            while(right <= high && nums[i] > 2L * nums[right]) right++;
+            count += right - (mid + 1);
+        }
         return count;
     }
+
     public int reversePairs(int[] nums) {
-        return mergeSort(nums, 0, nums.length - 1);
+        return (int) mergeSort(nums, 0, nums.length - 1);
     }
 }
