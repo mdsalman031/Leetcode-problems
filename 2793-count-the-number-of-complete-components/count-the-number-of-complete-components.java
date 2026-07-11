c60c900fbb31bbb64036f709ff4676802338ad09
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-07-12 00:13:19 +0530

    Time: 13 ms (30.91%) | Memory: 47.6 MB (76.09%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,51 +1,49 @@
 class Solution {
-    private boolean bfs(int V, List<List<Integer>> adj, boolean[] visited, int node) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(node);
-        visited[node] = true;
+    private boolean bfs(int node, List<List<Integer>> adjList, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
 
-        int nodes = 0;
-        int degreeSum = 0;
-        while(!queue.isEmpty()) {
-            int ele = queue.poll();
+        int nodes = 0, degreeSum = 0;
+        while(!q.isEmpty()) {
+            int u = q.poll();
             nodes++;
-            degreeSum += adj.get(ele).size();
+            degreeSum += adjList.get(u).size();
 
-            for(int neighbour : adj.get(ele)) {
-                if(!visited[neighbour]) {
-                    visited[neighbour] = true;
-                    queue.offer(neighbour);
+            for(int v : adjList.get(u)) {
+                if(!visited[v]) {
+                    visited[v] = true;
+                    q.add(v);
                 }
             }
         }
 
-        // Check if the subgraph is complete
         int edgeCount = degreeSum / 2;
-        if(edgeCount == nodes * (nodes - 1) / 2) return true; // formula for complete graph k*(k-1)/2
-        return false;
+        return edgeCount == (nodes * (nodes - 1) / 2);
     }
-    public int countCompleteComponents(int V, int[][] edges) {
-        // Step 1: Build adjacency list
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
 
-        for(int i = 0 ; i < edges.length ; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj.get(u).add(v);
-            adj.get(v).add(u); 
+    public int countCompleteComponents(int n, int[][] edges) {
+        List<List<Integer>> adjList = new ArrayList<>();
+
+        for(int i = 0 ; i < n ; i++) {
+            adjList.add(new ArrayList<>());
         }
-        // Step 2: BFS components
-        boolean[] visited = new boolean[V];
+
+        for(int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+
         int components = 0;
-        for(int i = 0 ; i < V ; i++) {
+        boolean[] visited = new boolean[n];
+        for(int i = 0 ; i < n ; i++) {
             if(!visited[i]) {
-                if(bfs(V, adj, visited, i)) {
-                    components++;
-                }
-                
+                visited[i] = true;
+                if(bfs(i, adjList, visited)) components++;
             }
         }
+
         return components;
     }
 }
