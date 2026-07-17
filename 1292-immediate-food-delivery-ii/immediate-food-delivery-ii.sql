679b96a7873400b92585b3a5c01913652402c979
MD Salman <155325365+mdsalman031@users.noreply.github.com>
2026-07-17 23:46:37 +0530

    Time: 607 ms (86.98%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -7,7 +7,7 @@ SELECT ROUND(
 FROM (
     SELECT *,
         ROW_NUMBER() OVER (
-            PARTITION BY customer_id ORDER BY order_date, customer_pref_delivery_date
+            PARTITION BY customer_id ORDER BY order_date
         ) AS rnk
     FROM Delivery
 ) t
